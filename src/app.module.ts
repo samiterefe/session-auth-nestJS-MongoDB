cae094aa8f3f20c1a1505a6e90ac8c29613eb8d3
samiterefe <samuelterefe2@gmail.com>
2022-11-29 14:54:08 +0300

    working with  user controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { urlString } from './configDb';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(urlString)],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(urlString),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  //post / signup
+  @Post('/signup')
+  async addUser(
+    @Body('password') userPassword: string,
+    @Body('username') userName: string,
+  ) {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(userPassword, saltOrRounds);
+    const result = await this.usersService.insertUser(userName, hashedPassword);
+    return {
+      msg: 'User successfully registered',
+      userId: result.id,
+      userName: result.username,
+    };
+  }
+}
