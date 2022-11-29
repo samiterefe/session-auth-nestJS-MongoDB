6bc2e4a03ef6ec60bb21b23d3dae9ceb91e9d2c3
samiterefe <samuelterefe2@gmail.com>
2022-11-29 16:48:22 +0300

    express-session-logout

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
   imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post, Request, UseGuards, } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import * as bcrypt from 'bcrypt';
+import { AuthenticatedGuard } from 'src/auth/ authenticated.guard';
+import { LocalAuthGuard } from 'src/auth/local.auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -22,8 +31,21 @@ export class UsersController {
     };
   }
   //Post / Login
+  @UseGuards(LocalAuthGuard)
   @Post('/login')
   login(@Request() req): any {
     return { User: req.user, msg: 'User logged in' };
+  }
+
+  // Get / protected
+  @UseGuards(AuthenticatedGuard)
+  @Get('/protected')
+  getHello(@Request() req): string {
+    return req.user;
+  }
+  @Get('/logout')
+  logout(@Request() req): any {
+    req.session.destroy();
+    return { msg: 'The user session has ended' };
   }
 }
